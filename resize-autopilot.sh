c18ccea5a018096d14984a30223485d826721268
JakubPawelczak <jakub.pawelczak.pl@gmail.com>
2025-09-19 11:51:34 +0000

    fixed resize-autopilot.sh

--- resize-autopilot.sh
+++ resize-autopilot.sh
@@ -9,15 +9,15 @@ sleep 1s
 
 echo "Let's start from scale-up CPU req and limits to 1.5 to get pod with 1.5 CPU & 1 GiB Mem" 
 echo ""
+
 kubectl patch pod resize-demo-g --subresource resize --patch \
-  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"cpu":"1.5"},"limits":{"cpu":"1.5"}}}]}}' \
-  && echo "scale-up CPU req and limits to 1.5"
+  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"cpu":"1.5"},"limits":{"cpu":"1.5"}}}]}}'
 
 echo ""
 echo "Error: the resize was blocked as Autopilot accepts CPU:Mem ratio between 1:1 and 1:6.5"
 echo ""
 
-sleep 1s
+sleep 5s
 
 kubectl patch pod resize-demo-g --subresource resize --patch \
   '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"memory":"2G"},"limits":{"memory":"2G"}}}]}}' \
@@ -27,8 +27,8 @@ echo ""
 sleep 5s
 
 kubectl patch pod resize-demo-g --subresource resize --patch \
-  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"memory":"1G"},"limits":{"memory":"1G"}}}]}}' \
-  && echo "scale-down Mem req and limits back to 1G"
+  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"memory":"1.5G"},"limits":{"memory":"1.5G"}}}]}}' \
+  && echo "scale-down Mem req and limits to 1.5G"
 
 echo ""
 echo "Note: IPPR supports in-place Mem scale-down starting from GKE 1.34"
@@ -44,8 +44,8 @@ echo ""
 sleep 5s
 
 kubectl patch pod resize-demo-g --subresource resize --patch \
-  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"cpu":"0.250","memory":"0.250G"},"limits":{"cpu":"0.250","memory":"0.250G"}}}]}}' \
-  && echo "scale-down to 0.250 CPU and to 0.250G Mem"
+  '{"spec":{"containers":[{"name":"demo-g", "resources":{"requests":{"cpu":"0.250","memory":"0.600G"},"limits":{"cpu":"0.250","memory":"0.600G"}}}]}}' \
+  && echo "scale-down to 0.250 CPU and to 0.600 Mem"
 
 echo ""
 sleep 5s
